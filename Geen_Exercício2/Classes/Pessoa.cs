ab687ba26848c18002a007ed7616ea30dc2c7b4d
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-06-07 10:09:10 -0300

    Classe Pessoa

---
+++ Geen_Exercício2/Classes/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geen_Exercício2.Classes
+{
+    class Pessoa
+    {
+        public String Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoa()
+        {
+        }
+
+        public Pessoa(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+    }
+}
